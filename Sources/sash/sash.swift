82d3940111c5e1400c7cb6c9477e2253af126fc9
Arphd4 <aarav.kharade1234@gmail.com>
2026-09-17 20:06:04 +0530

    Update license author

--- Sources/sash/sash.swift
+++ Sources/sash/sash.swift
@@ -1,4 +1,4 @@
-// Copyright 2026 ArkOS Authors
+// Copyright 2026 Aarav Ravindra Kharade
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
